bbe7b379c1f1780b4050482029e9af61fe74dfa0
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-01-21 17:42:25 -0300

    update JS

---
+++ js/mouse.js
@@ -0,0 +1,12 @@
+
+let mouse = new Mouse({
+    mouse: 'point',
+    color: '#000000',
+    el: 'a , button',
+    mouseHover: 'background',
+    mousePointer: false,
+    el_hover: true,
+    mouse_clicked: true,
+    clrChanged: false
+})
+mouse.mouse()
